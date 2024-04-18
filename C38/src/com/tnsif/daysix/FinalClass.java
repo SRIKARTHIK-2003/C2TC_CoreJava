b12dd3173ee541010aedbfc721187066005472ca
ELCOT <ELCOT@SRIKARTHIK.mshome.net>
2024-04-18 10:27:41 +0530

    day six programs

---
+++ C38/src/com/tnsif/daysix/FinalClass.java
@@ -0,0 +1,8 @@
+package com.tnsif.daysix;
+
+final class FinalClass {
+void show() {
+System.out.println("Final class cannot be inherited");
+}
+}
+//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
